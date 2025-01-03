de70fd66c271812c6ce0da3f30addd2baaf06bc2
CodingManoj <CodingManoj@gmail.com>
2025-01-02 22:57:07 -0500

    Building Helm Charts

---
+++ argo-jobs.sh
@@ -0,0 +1,21 @@
+echo "Authenticaing to EKS ${1}"
+aws eks update-kubeconfig --name ${1}-eks
+kubectl get nodes 
+
+ARGO_URL="argocd.cloudapps.today"
+ARGO_PWD=$(kubectl get secret argocd-initial-admin-secret -n argocd-initial-admin-secret -o json -n argocd | jq '.data.password'| xargs | base64 -d)
+argocd login $ARGO_URL  --username admin --password $ARGO_PWD
+
+argocd app list |grep "argocd/${2}"
+if [ $? -ne 0 ]; then
+    echo "creating ${1} ${2} app"
+    argocd app create ${2} --repo https://github.com/B58-CloudDevOps/expense-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values ${1}/${2}.yaml --sync-policy auto  --grpc-web --helm-set imageTag=${3}
+    argocd app wait ${2}
+fi 
+
+echo "Deployment of ${2} ${3} version to ${1} eks cluster" 
+argocd app set ${2} --parameter imageTag=${3}
+
+echo "Deployment of version ${3} is completed"
+argocd app wait ${2}
+kubectl get pods | grep ${2}
