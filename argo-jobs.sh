5a01ee21e7911d4250529a1a4369e31aa41a8c2b
CodingManoj <CodingManoj@gmail.com>
2025-01-02 23:00:44 -0500

    Building Helm Charts

--- argo-jobs.sh
+++ argo-jobs.sh
@@ -9,9 +9,9 @@ argocd login $ARGO_URL  --username admin --password $ARGO_PWD
 argocd app list |grep "argocd/${2}"
 if [ $? -ne 0 ]; then
     echo "creating ${1} ${2} app"
-    argocd app create ${2} --repo https://github.com/B58-CloudDevOps/expense-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values ${1}/${2}.yaml --sync-policy auto  --grpc-web --helm-set imageTag=${3}
+    argocd app create ${2} --repo https://github.com/B58-CloudDevOps/roboshop-helmcharts.git --path ./chart --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${1}/${2}.yaml --sync-policy auto  --grpc-web --helm-set imageTag=${3}
     argocd app wait ${2}
-fi 
+fi
 
 echo "Deployment of ${2} ${3} version to ${1} eks cluster" 
 argocd app set ${2} --parameter imageTag=${3}
